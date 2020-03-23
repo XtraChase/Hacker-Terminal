0a3c3c7a7d6ba65fe55820599d6f4cd2444d56f6
VirtualImagery <james.chase.richards@gmail.com>
2020-03-23 15:54:45 -0400

    Terminal greeting set up

---
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+
+ 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu ()
+    {
+        Terminal.ClearScreen(); 
+        string greeting = "Select a hacking objective";
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("Press 1 for the police station");
+        Terminal.WriteLine("press 2 for the FBI");
+        Terminal.WriteLine("Press 3 for Area 51");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
