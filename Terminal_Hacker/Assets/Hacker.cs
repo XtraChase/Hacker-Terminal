847103c3bf77f35e56f55c5decffe27e8c4adca5
VirtualImagery <james.chase.richards@gmail.com>
2020-04-07 15:38:35 -0400

    Added level design

--- Terminal_Hacker/Assets/Hacker.cs
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -1,37 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hacker : MonoBehaviour
-
- 
-{
+public class Hacker : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
        
-        ShowMainMenu("Select a hacking objective");
-        print("Hello" + "World");
+        ShowMainMenu();
     }
 
-    void ShowMainMenu (string greeting)
-    {
+    void ShowMainMenu () {
         Terminal.ClearScreen(); 
-        Terminal.WriteLine(greeting);
-        Terminal.WriteLine("Press 1 for the police station");
+        Terminal.WriteLine("Press 1 for the Police Station");
         Terminal.WriteLine("press 2 for the FBI");
         Terminal.WriteLine("Press 3 for Area 51");
         Terminal.WriteLine("Enter your selection:");
     }
+     // game state
+    int level;
 
-    void OnUserInput(string input)
+    void OnUserInput(string  input) {
+        if (input == "menu") {
+            ShowMainMenu();
+        }
+        else if(input == "1") {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else {
+            Terminal.WriteLine("Error");
+        }
+    }
+
+    void StartGame()
     {
-        print("The user typed " + input);
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Begin Hacking the Police Station");
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
